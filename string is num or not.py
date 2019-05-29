2ef81a9630bc7673ae45f3e3067318fcdd3bbad1
PRADEEPKUMARJEYAKUMAR <51090588+PRADEEPKUMARJEYAKUMAR@users.noreply.github.com>
2019-05-29 20:44:01 +0530

    Update string is num or not.py

--- string is num or not.py
+++ string is num or not.py
@@ -1,8 +1,8 @@
 n=input()
-l=['1','2','3','4','5','6','7','8','9','0','.']
+p=['1','2','3','4','5','6','7','8','9','0','.']
 for i in n:
-    if i not in l:
-        print("no")
+    if i not in p:
+        print("No")
         break
 else:
-    print("yes")
+    print("Yes")
