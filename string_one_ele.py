2b86ab70a21843a0c0dabe8d3b912e87fa8d578a
PRADEEPKUMARJEYAKUMAR <51090588+PRADEEPKUMARJEYAKUMAR@users.noreply.github.com>
2019-05-30 18:13:52 +0530

    Update string_one_ele.py

--- string_one_ele.py
+++ string_one_ele.py
@@ -1,8 +1,9 @@
 a,b=list(map(str,input().split()))
-if((len(a)>len(b)):
+if((len(a)==len(b)):
     print(a)
-elif(len(a)<len(b)):
-    print(b)
 else:
-    print(a)
+    if(len(a)<len(b)):
+        print(b)
+    else:
+        print(a)
     
