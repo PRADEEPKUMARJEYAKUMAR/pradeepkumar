eca761261d23d42bb7e8c22dd9257e5e5a21e6c8
PRADEEPKUMARJEYAKUMAR <51090588+PRADEEPKUMARJEYAKUMAR@users.noreply.github.com>
2019-05-30 19:10:26 +0530

    Update find_power_of_2.py

--- find_power_of_2.py
+++ find_power_of_2.py
@@ -1,10 +1,9 @@
 n=int(input())
 for i in range(1,n):
     s=2**i
-    if(i>1):
-        if s==n:
-            print("yes")
-            break
+    if(i>1 and s==n):
+        print("yes")
+        break
 else:
     print("no")
     
